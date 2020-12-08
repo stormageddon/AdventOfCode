6a7c40cd247e491c4777a59e88da855d9a77690b
Mike Caputo <mike.caputo@asynchrony.com>
2020-12-08 14:26:19 -0600

    remove extraneous return statements

--- 2020/day6/day6.js
+++ 2020/day6/day6.js
@@ -1,12 +1,14 @@
-import { promises as fs } from 'fs';
+import { promises as fs } from "fs";
 
 async function getData() {
-    const data = await fs.readFile('input.txt', 'utf-8');
+  const data = await fs.readFile("input.txt", "utf-8");
 
-    return data.toString().replace(/\n\r/g, "\n")
+  return data
+    .toString()
+    .replace(/\n\r/g, "\n")
     .replace(/\r/g, "\n")
     .split(/\n{2,}/g)
-    .map((str) => str.replace(/\n/g, ""))
+    .map((str) => str.replace(/\n/g, ""));
 }
 
-console.log((await getData()).map((_) => {return [...new Set(_.split(""))].length}).reduce((p, c) => { return p + c }))
+console.log((await getData()).map((_) => [...new Set(_.split(""))].length).reduce((p, c) => p + c));
